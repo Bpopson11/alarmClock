ceda01140453f66152e8bad23f37d21d8d1b4f20
Joe Karasek <joseph.karasek@gmail.com>
2016-03-14 09:45:08 -0700

    update notes

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Plugins used by gulp
+var browserify = require('browserify');
+var del = require('del');
+var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var source = require('vinyl-source-stream');
+
+// Set build env from command line
+var buildProduction = utilities.env.production;
+
+// clean files
+gulp.task('clean', function(){
+  return del(['build', 'tmp']);
+});
+
+// linter
+gulp.task('jshint', function(){
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// concat all js files, puts in tmp
+gulp.task('concat', function() {
+  return gulp.src(['./js/*.js'])
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest('./tmp'));
+});
+
+// Takes concatenated JS and browserify's it
+// using a second arguement with gulp.task, we are passing in an array of task dependencies -> tasks to run first for this task to work
+gulp.task('jsBrowserify' , ['concat'] , function() {
+  return browserify({ entries: ['./tmp/allConcat.js']})
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build/js'));
+});
+
+// minify
+gulp.task('minifyScripts', ['jsBrowserify'] , function() {
+  return gulp.src('./build/js/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+// build, based on production environment
+gulp.task('build', ['clean'] , function() {
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+
+// gulp watch files for changes
+gulp.task('watch', function() {
+  gulp.watch('js/*.js', ['jshint' , 'build']);
+});
+
+// default action
+gulp.task('default', ['lint', 'build', 'watch']);
