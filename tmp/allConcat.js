e2ccd8529333be5193769866765e8fa599daadf6
Bri Popson and Joseph Karasek <joseph.karasek@gmail.com>
2016-03-15 09:07:53 -0700

    add disply of active time

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
   // Puts the time on the page
-  $('#time').text(moment().format("MMMM Do YYYY, h:mm a"));
+  setInterval(function() {
+    $('#time').text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  }, 100);
 
 });
