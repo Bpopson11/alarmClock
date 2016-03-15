0bbdd5d56275bec684021a5813c8c5a29195c0a0
Bri Popson and Joseph Karasek <joseph.karasek@gmail.com>
2016-03-15 11:33:40 -0700

    add toggle of alarm display

--- js/browser.js
+++ js/browser.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
   var alarm = null;
+  $('#alarm_reset').hide();
 
   // A trigger for submit of alarm form, sets a value for an alarm time
   $('#alarm').submit(function(event) {
@@ -12,9 +13,14 @@ $(document).ready(function(){
     $('#alarm').hide();
     // populate the time to display for user what alarm time they set
     $('#alarm_set_time').text('Alarm has been set for ' + alarm.toLocaleTimeString() + '.');
+    $('#alarm_reset').show();
   });
 
-
+  $('#alarm_reset').submit(function() {
+    alarm = null;
+    $('#alarm').show();
+    $('#alarm_reset').hide();
+  });
 
   // refresh display and check alarm function
   var refreshAndCheck = function() {
