37f1a2dd96a1615ee051ef308719d1a8f77899d8
Bri Popson and Joseph Karasek <bpopson@gmail.com>
2016-03-15 14:56:12 -0700

    added weather API, including display of current weather and temp

---
+++ temp.md
@@ -0,0 +1,1 @@
+http://api.openweathermap.org/data/2.5/weather?lat=-45&lon=-120&appid=27b0c263f640ffcf06a8e01b620e6172
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -61,3 +61,14 @@ $(document).ready(function(){
 
 
 });
+
+var apiKey = "27b0c263f640ffcf06a8e01b620e6172";
+
+$(document).ready(function() {
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?zip=97204,US&appid=' + apiKey, function(response) {
+    $('#weather').text('Currently, the weather is ' + response.weather[0].description);
+    console.log(response.weather[0].description);
+    $('#temperature').text(Math.floor(response.main.temp * 9/5 - 459.67) + " degrees Fahrenheit");
+  });
+});
