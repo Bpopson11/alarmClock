58d4c9ca8f2df8715fc131fcdf33045039826580
Bri Popson and Joseph Karasek <joseph.karasek@gmail.com>
2016-03-15 09:04:56 -0700

    add display time with moment

--- js/signup-interface.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('#form').submit(function(event){
-    event.preventDefault();
-    var email = $('#email').val();
-    $('#email').hide();
-    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
-  });
-});
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,24 +1,6 @@
-var pingPong = require('./../js/ping-pong.js').pingPong;
-
 $(document).ready(function(){
-  $('#pingpong').submit(function(event){
-    event.preventDefault();
 
-    var goal = $('#goal').val();
-    var result = pingPong(goal);
-    result.forEach(function(element){
-      $('#solution').append("<li>" + element + " this is an edit</li>");
-    });
+  // Puts the time on the page
+  $('#time').text(moment().format("MMMM Do YYYY, h:mm a"));
 
-
-  });
-});
-
-$(document).ready(function(){
-  $('#form').submit(function(event){
-    event.preventDefault();
-    var email = $('#email').val();
-    $('#email').hide();
-    $('#solution').prepend('<p>Thank you, ' + email + ' has been added to our list!</p>');
-  });
 });
